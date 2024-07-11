eb92c6f07454ede49542afd98c78d645ba9e4172
Imtiaz Shawn <imtiazshawn01@gmail.com>
2024-07-11 13:05:24 +0600

    solving the inital date setting

--- app/src/main/java/com/example/trackerexodon/viewmodels/ExpenseViewModel.kt
+++ app/src/main/java/com/example/trackerexodon/viewmodels/ExpenseViewModel.kt
@@ -1,12 +1,11 @@
-import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class ExpenseViewModel : ViewModel() {
     val title = mutableStateOf("")
     val amount = mutableStateOf("")
-    val date = mutableStateOf("")
+    val date = mutableStateOf(Date().time.toString())
     val category = mutableStateOf("")
     val type = mutableStateOf("")
 
